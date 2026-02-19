30af6eaaff3951ee880543fd47fa92f9ace82e70
Tomoaki Masuda <masuda@moonmile.net>
2026-02-19 09:50:08 +0900

    権限を付ける前に戻し

--- folkbears-transmitter-droid/app/src/main/java/net/moonmile/folkbears/transmitter/MainActivity.kt
+++ folkbears-transmitter-droid/app/src/main/java/net/moonmile/folkbears/transmitter/MainActivity.kt
@@ -1,14 +1,20 @@
 package net.moonmile.folkbears.transmitter
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
@@ -17,20 +23,23 @@ import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import net.moonmile.folkbears.transmitter.ui.theme.FolkbearsTransmitterTheme
+import androidx.core.content.ContextCompat
 import net.moonmile.folkbears.transmitter.service.BeaconTransmitter
 import net.moonmile.folkbears.transmitter.service.ENSimTransmitter
 import net.moonmile.folkbears.transmitter.service.GattAdvertise
 import net.moonmile.folkbears.transmitter.service.ManufacturerDataTransmitter
+import net.moonmile.folkbears.transmitter.ui.theme.FolkbearsTransmitterTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
